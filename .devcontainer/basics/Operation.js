4fd4ef6727bf08a80c48a3042d2b7287f3ad290a
Sandip Najan <80146714+SandipNajan@users.noreply.github.com>
2023-12-26 08:03:42 +0000

    operation

---
+++ .devcontainer/basics/Operation.js
@@ -0,0 +1,34 @@
+let value = 3
+let negValue = -value
+//console.log(negValue)  //-3
+
+let str1 = "Hello"
+let str2 = "Sandip"
+let str3 = str1 + str2
+// console.log(str3);
+// console.log("1" + 3);  //if string came first then everyting will be in string if number came first then perform operation up to string
+// console.log(1 + "3");  //
+// console.log(1 + 3 + "3")
+// console.log((4 + 3) * 5 % 2)   
+
+let gameCounter = 200
+//gameCounter ++  ==>201
+++gameCounter
+console.log(gameCounter)
+
+/* link to study - 
+https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+*/
+
+//Comparison Operation
+console.log("**** Comparisopn opetation **")
+console.log("3" > 2)
+console.log(null>0)
+console.log(null<0)
+console.log(null == 0)
+console.log(undefined == 0)
+
+
+// ===
+console.log("2" == 2)
